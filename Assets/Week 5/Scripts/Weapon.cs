73a4e82cf6ad1a4237184094d80a0a602e4929de
Leon-Gu <guyunx@sheridancollege.ca>
2024-02-08 16:43:54 -0500

    Task 4a complete, the spear can fly and hurt the knight

---
+++ Assets/Week 5/Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float speed = 5f;
+    Rigidbody2D rb;
+    Vector2 move = new Vector2(1, 0);
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer + 1f * Time.deltaTime;
+        if (timer > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * speed * Time.deltaTime);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Knight")
+        {
+            collision.gameObject.SendMessage("TakeDamage", 1);
+            Destroy(gameObject);
+        }
+    }
+
+
+}
