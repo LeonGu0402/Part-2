9d2408a17cd73e580fad0c2be8efda758830600e
Leon-Gu <guyunx@sheridancollege.ca>
2024-02-22 17:31:03 -0500

    Task4a complete, there is a goal keeper

---
+++ Assets/Week 7/Scripts/GoalkeeperController.cs
@@ -0,0 +1,44 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalkeeperController : MonoBehaviour
+{
+    public Rigidbody2D goalKeeper;
+    public float distance;
+    Vector2 direction;
+    Vector2 playerPosition;
+    Vector2 goalLinePosition;
+    public float goalRadius = 2;
+
+    private void FixedUpdate()
+    {
+        if (Controller.SelectedPlayer == null) return;
+        if (distance < goalRadius)
+        {
+            goalKeeper.position = goalLinePosition - direction * distance;
+        }
+        else if(distance > goalRadius)
+        {
+            goalKeeper.position = goalLinePosition - direction * goalRadius;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Controller.SelectedPlayer == null) return;
+        playerPosition = (Vector2)Controller.SelectedPlayer.transform.position;
+        goalLinePosition = (Vector2)transform.position;
+
+        direction = goalLinePosition - playerPosition;
+        distance = direction.magnitude / 2;
+        direction.Normalize();
+
+
+
+    }
+}
