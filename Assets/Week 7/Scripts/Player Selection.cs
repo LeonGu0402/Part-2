9817efefe525ae1b200445c4e63057e8628443dd
Leon-Gu <guyunx@sheridancollege.ca>
2024-02-22 14:22:15 -0500

    Task 1b complete, we can select player

---
+++ Assets/Week 7/Scripts/Player Selection.cs
@@ -0,0 +1,48 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerSelection : MonoBehaviour
+{
+
+    public Color selectColour;
+    public Color unselectColour;
+    SpriteRenderer sr;
+    Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        Selected(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Selected(bool isSelected)
+    {
+        if (isSelected)
+        {
+            sr.color = selectColour;
+        }
+        else if (!isSelected)
+        {
+            sr.color=unselectColour;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Selected(true);
+    }
+}
